af3e0699c926ccb4b4f4680d3d0fd132e568be65
Charade000 <80553647+Charade000@users.noreply.github.com>
2021-03-18 14:57:40 +0000

    Full Screen pt.3
    Expanding full screen functionality to all pages and changing it a bit.

--- Main.py
+++ Main.py
@@ -3,12 +3,14 @@ from tkinter import *
 class Home():
     def __init__(self, master):
         self.master = master
-        self.master.geometry('610x780')
         self.master.title("Taxi And Minibus")
         self.master.configure(background='turquoise3')
-        self.master.attributes("-fullscreen", True)
         self.master.bind("<F11>", self.toggle_fullscreen)
         self.master.bind("<Escape>", self.end_fullscreen)
+        
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        
 
         self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Login_B=Button(self.master,text='Login',command=self.login,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
@@ -16,7 +18,7 @@ class Home():
         self.About_B=Button(self.master,text='About',command=self.about,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=2)
 
     def toggle_fullscreen(self, event=None):
-        self.state = not self.state   # Just toggling the boolean
+        self.state = not self.state 
         self.master.attributes("-fullscreen", self.state)
         return "break"
 
@@ -28,19 +30,19 @@ class Home():
     def login(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=loginWindow(root2)
 
     def sign_up(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=signupWindow(root2)
     
     def about(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=aboutWindow(root2)
 
 
@@ -51,21 +53,38 @@ class loginWindow():
         self.master.title("Login")
         self.master.configure(background='turquoise3')
         
+        self.master.bind("<F11>", self.toggle_fullscreen)
+        self.master.bind("<Escape>", self.end_fullscreen)
+        
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        
+
+        self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Back_B=Button(self.master,text='Back',command=self.back,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
         self.adminButton=Button(self.master,text='Admin',command=self.adminlogin,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=1)
         self.LoginButton=Button(self.master,text='Login',command=self.adminlogin,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=2)
 
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state 
+        self.master.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        return "break"
         
     def back(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=Home(root2)
     
     def adminlogin(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
         muGUI=Admin_Window(root2)
         
 class Admin_Window():
@@ -74,6 +93,25 @@ class Admin_Window():
         self.master.geometry('610x780')
         self.master.title("Admin Login")
         self.master.configure(background='turquoise3')
+        
+        self.master.bind("<F11>", self.toggle_fullscreen)
+        self.master.bind("<Escape>", self.end_fullscreen)
+        
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        
+
+        self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
+
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state 
+        self.master.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        return "break"
 
 class signupWindow():
     def __init__(self, master):
@@ -82,12 +120,30 @@ class signupWindow():
         self.master.title("Sign Up")
         self.master.configure(background='turquoise3')
         
+        self.master.bind("<F11>", self.toggle_fullscreen)
+        self.master.bind("<Escape>", self.end_fullscreen)
+        
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        
+
+        self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Back_B=Button(self.master,text='Back',command=self.back,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
+
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state 
+        self.master.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        return "break"
     
     def back(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=Home(root2)
         
 
@@ -97,13 +153,29 @@ class aboutWindow():
         self.master.geometry('610x780')
         self.master.title("About")
         self.master.configure(background='turquoise3')
+        self.master.bind("<F11>", self.toggle_fullscreen)
+        self.master.bind("<Escape>", self.end_fullscreen)
         
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        
+        self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Back_B=Button(self.master,text='Back',command=self.back,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
+
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state 
+        self.master.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.master.attributes("-fullscreen", False)
+        return "break"    
     
     def back(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
-        root2.resizable(width=False, height=False)
+        root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=Home(root2)
 
 
