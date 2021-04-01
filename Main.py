5ad946646a0104886e11c0a5f6d8b9b773142c91
Charade000 <80553647+Charade000@users.noreply.github.com>
2021-04-02 00:14:24 +0100

    Adding Comments

--- Main.py
+++ Main.py
@@ -5,28 +5,31 @@ class Home():
         self.master = master
         self.master.title("Taxi And Minibus")
         self.master.configure(background='turquoise3')
+        
+        # Binding F11 and ESCAPE keys to full screen
         self.master.bind("<F11>", self.toggle_fullscreen)
         self.master.bind("<Escape>", self.end_fullscreen)
-        
         self.state = False
         self.master.attributes("-fullscreen", False)
         
 
+        # 'Home' Page Buttons
         self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Login_B=Button(self.master,text='Login',command=self.login,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
         self.SignUp_B=Button(self.master,text='Sign Up',command=self.sign_up,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=1)
-        self.About_B=Button(self.master,text='About',command=self.about,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=2)
+        self.About_B=Button(self.master,text='About Us',command=self.about,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=2)
 
+    # Toggling full screen
     def toggle_fullscreen(self, event=None):
         self.state = not self.state 
         self.master.attributes("-fullscreen", self.state)
         return "break"
-
     def end_fullscreen(self, event=None):
         self.state = False
         self.master.attributes("-fullscreen", False)
         return "break"
     
+    # Initializing New Windows
     def login(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
@@ -52,40 +55,45 @@ class loginWindow():
         self.master.title("Login")
         self.master.configure(background='turquoise3')
         
+        # Binding F11 and ESCAPE keys to full screen
         self.master.bind("<F11>", self.toggle_fullscreen)
         self.master.bind("<Escape>", self.end_fullscreen)
-        
         self.state = False
         self.master.attributes("-fullscreen", False)
         
 
+        # Login Page Buttons
         self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Back_B=Button(self.master,text='Back',command=self.back,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
         self.LoginButton=Button(self.master,text='Login',command=self.checkLogin,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=2)
         
+        # Login Page Labels
         self.emailLabel=Label(self.master,text='Email',bg='turquoise3',font='Bembo',fg='black').grid(row=2,column=0,pady=5)
         self.passwordLabel=Label(self.master,text='Password',bg='turquoise3',font='Bembo',fg='black').grid(row=3,column=0,pady=5)
         
+        # Entry Page Entry
         self.emailEntry=Entry(self.master,bg='PaleTurquoise1',bd=0,font='Bembo',fg='black',width=15).grid(row=2,column=2,pady=5)
         self.passwordEntry=Entry(self.master,bg='PaleTurquoise1',bd=0,font='Bembo',fg='black',width=15).grid(row=3,column=2,pady=5)
 
-    
+
+    # Toggling full screen
     def toggle_fullscreen(self, event=None):
         self.state = not self.state 
         self.master.attributes("-fullscreen", self.state)
         return "break"
-
     def end_fullscreen(self, event=None):
         self.state = False
         self.master.attributes("-fullscreen", False)
         return "break"
         
+    # Return To 'Home' Screen
     def back(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
         root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
         muGUI=Home(root2)
     
+    # Connecting To Database To Check Login
     def checkLogin(self):
         
         pass
@@ -103,26 +111,27 @@ class signupWindow():
         self.master.title("Sign Up")
         self.master.configure(background='turquoise3')
         
+        # Binding F11 and ESCAPE keys to full screen
         self.master.bind("<F11>", self.toggle_fullscreen)
         self.master.bind("<Escape>", self.end_fullscreen)
-        
         self.state = False
         self.master.attributes("-fullscreen", False)
         
-
+        # Sign Up Page Buttons
         self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Back_B=Button(self.master,text='Back',command=self.back,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
 
+    # Toggling full screen
     def toggle_fullscreen(self, event=None):
         self.state = not self.state 
         self.master.attributes("-fullscreen", self.state)
         return "break"
-
     def end_fullscreen(self, event=None):
         self.state = False
         self.master.attributes("-fullscreen", False)
         return "break"
     
+    # Return To 'Home' Screen
     def back(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
@@ -133,33 +142,35 @@ class signupWindow():
 class aboutWindow():
     def __init__(self, master):
         self.master = master
-        self.master.title("About")
+        self.master.title("About Us")
         self.master.configure(background='turquoise3')
+        
+        # Binding F11 and ESCAPE keys to full screen
         self.master.bind("<F11>", self.toggle_fullscreen)
         self.master.bind("<Escape>", self.end_fullscreen)
-        
         self.state = False
         self.master.attributes("-fullscreen", False)
         
+        # About Window Buttons
         self.Full_B=Button(self.master,text='Full Screen',command=self.toggle_fullscreen,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=3,padx=20)
         self.Back_B=Button(self.master,text='Back',command=self.back,bg='PaleTurquoise1',activebackground='turquoise3',bd=0,font='Bembo',fg='black',height=7,width=18).grid(row=1,column=0)
 
+    # Toggling full screen
     def toggle_fullscreen(self, event=None):
         self.state = not self.state 
         self.master.attributes("-fullscreen", self.state)
         return "break"
-
     def end_fullscreen(self, event=None):
         self.state = False
         self.master.attributes("-fullscreen", False)
         return "break"    
     
+    # Return To 'Home' Screen
     def back(self):
         self.master.withdraw()
         root2=Toplevel(self.master)
         # root2.geometry("200x{1}+0+0".format(root2.winfo_screenheight()))
         root2.geometry("{0}x{1}+0+0".format(root2.winfo_screenwidth(), root2.winfo_screenheight()))
-
         muGUI=Home(root2)
 
 
